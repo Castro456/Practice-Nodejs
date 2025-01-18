fa7ac712e17a5414ab0151a91be8b577467baef4
Castro456 <castrosid456@gmail.com>
2025-01-18 21:00:15 +0530

    http routes

--- modules/httpIndex.js
+++ modules/httpIndex.js
@@ -14,9 +14,20 @@ const server = http.createServer((req, res) => { // res is the server response |
 
   // let htmlFile = fs.createReadStream(__dirname + '/index.html').pipe(res) // This is the optimized way to display html file.
 
-  let htmlFile = fs.readFileSync('./index.html', 'utf8') // This is the optimized way
-  htmlFile = htmlFile.replace("{{name}}", responseData.first_name)
-  res.end(htmlFile)
+  if(req.url === '/') { // req.url will return the endpoints
+    let htmlFile = fs.readFileSync('./index.html', 'utf8') // This is the optimized way
+    htmlFile = htmlFile.replace("{{name}}", responseData.first_name)
+    res.end(htmlFile)
+  }
+  else if(req.url === '/about') {
+    let htmlFile = fs.readFileSync('./index.html', 'utf8') // This is the optimized way
+    htmlFile = htmlFile.replace("{{name}}", responseData.last_name)
+    res.end(htmlFile)
+  }
+  else {
+    // res.writeHead(404)
+    res.end('Page not found')
+  }
 })
 
 server.listen((3000), () => { // listen to this port
